e82e0e6f8fca53f4bfc69c9d24f5ab59f004d5af
luoluo <625749330@qq.com>
2024-02-22 11:25:36 +0800

    rollup安装

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+export default{
+    input:"./src/index.ts",
+    output:[
+        {
+            format:"cjs",
+            file:"lib/guide-micro-vue.cjs.js"
+        },
+        {
+            format:"es",
+            file:"lib/guide-micro-vue.esm.js"
+        }
+    ],
+
+    plugins:[
+        
+    ]
+}
